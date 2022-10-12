aa5982e46e06dab9cae4de078ec8d78969c37191
Noah Wang <noahwang@169-231-109-154.wireless.ucsb.edu>
2022-10-12 15:53:46 -0700

    initialize

--- assort/List.cpp
+++ assort/List.cpp
@@ -1,1 +1,46 @@
 #include "List.h"
+ 
+List::List() {
+   head = nullptr;
+};
+List::List(const List& other) {
+ 
+};
+List::List(List&& other) {
+ 
+};
+List::~List() {
+ 
+};
+size_t List::count() const {
+   return 1;
+};
+void List::insert(const std::string& value) {
+   return;
+};
+const std::string& List::lookup(size_t index) const {
+  
+};
+void List::print(bool reverse = false) const {
+ 
+};
+std::string List::remove(size_t index) {
+ 
+};
+size_t List::remove(const std::string& value) {
+  
+};
+ 
+ 
+ 
+// List(); //Constructor
+//     List(const List& other); // Copy
+//     List(List&& other); // Move
+//     ~List(); //Deconstructor
+ 
+   // size_t             count() const;
+   // void               insert(const std::string& value);
+   // const std::string& lookup(size_t index) const;
+   // void               print(bool reverse = false) const;
+   // std::string        remove(size_t index);
+   // size_t             remove(const std::string& value);
