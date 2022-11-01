beb443c3acea8156613aca89cd5d7f1248baae7a
Noah Wang <noahwang@169-231-97-66.wireless.ucsb.edu>
2022-10-31 17:05:06 -0700

    ef

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -6,9 +6,10 @@
 
 //This function finds the person with name and returns its pointer.
 Person* GenePool::find(const std::string& name) const {
-    Person* node = new Person;
-    std::string stump = name;
-    return node;
+    if (Person* temp = GeneMap.find(name)->second) {
+        return temp;
+    }
+    return nullptr;
 };
 
 //This function takes in a tsv file in the form of a istream and is suppose to store it. Like parse in previous lab.
@@ -18,23 +19,18 @@ GenePool::GenePool(std::istream& stream) {
     std::string line;
     while (std::getline(stream, line))
     {
-        // std::cout << line;
-        // std::cout << "\n";
-        // continue;
         // If the line starts with # or is empty, then go to next line.
         if(line == "" || line[0] == '#') {
             continue;
         }
+
         Person* newNode = new Person;
+
         //Loop through each token seperated by a tab character.
         std::stringstream nLine(line);
-        // std::string currentToken;
-        // std::string tempName;
-        size_t count = 0;
-        //should be a way other than getLine to 
 
         std::string theirName, theirGender, theirMother, theirFather;
-        if (!(nLine >> theirName >> theirGender >> theirMother >> theirFather)) { break; } // error
+        if (!(nLine >> theirName >> theirGender >> theirMother >> theirFather)) { break; } // error (may not be needed if already checked in main)
         newNode->nameV = theirName; //Set Name
         if(theirFather == "???") { //Set Father Pointer, and set Father's children pointer to this node
             newNode->fatherV = nullptr;
@@ -57,86 +53,14 @@ GenePool::GenePool(std::istream& stream) {
             newNode->genderV = Gender::FEMALE;
         }
         GeneMap.insert({theirName, newNode});
-
-
-        // while(std::getline(nLine, currentToken, '\t')) {
-            // std::cout << currentToken;
-            // std::cout << "\n";
-            // continue;
-        //     if (count == 0) { //set nameV
-        //         newNode->nameV = currentToken;
-        //     }
-        //     else if (count == 2) { //set mother name... needs to look up mother in the map and set the pointer to them. feck?
-        //         if(currentToken == "???") {
-        //             newNode->motherV = nullptr;
-        //         }
-        //         else {
-        //             newNode->motherV = GeneMap.find(currentToken)->second;
-        //         }
-        //     }
-        //     else if (currentToken == "male") {
-        //         // Gender ge;
-        //         // newNode->genderV = Gender MALE;
-        //     }
-        //     else if(currentToken == "female") {
-        //         // newNode->genderV = FEMALE;
-        //     }
-        //     else { //set father name
-        //         if(currentToken == "???") {
-        //             newNode->fatherV = nullptr;
-        //         }
-        //         else {
-        //             newNode->fatherV = GeneMap.find(currentToken)->second;
-        //         }
-        //     }
-        //     count++;
-        // }
-        // GeneMap.insert({tempName, newNode});
     }
 };
 
 //Destructor. Should destruct all Person nodes inside it as well.
 GenePool::~GenePool() {
-
+    //loop through the map to delete all nodes in it!
+    for (auto const& x : GeneMap)
+    {
+        delete x.second;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //Find how many tokens there are in the line, in order to see how many tokens are part of their name (Some names are like Windsor Edward Batten-Barrow Smith)
-        // std::istringstream mLine(line);
-        // size_t count = std::distance(std::istream_iterator<std::string>(mLine), std::istream_iterator<std::string>());
-        
-        //Tokenize the line into seperate strings and then loop through them.
-        // std::istringstream nLine(line);
-        // std::string arrayTokens[count];
-        // Gender gender;
-        // Person* father;
-        // Person* mother;
-
-        //Put each token of the line into the array.
-        // for (int i = 0; i < count; i++) {
-        //     if (!(nLine >> arrayTokens[count])) { break; }
-        // }
-
-        // if (!(nLine >> arrayTokens[0] >> arrayTokens[1])) { break; } 
-        //process pair (a,b)
-        // while(nLine >> currentToken) {
-        //     currentCount++;
-        //     if(currentCount > count) {
-
-        //     }
-        // }
