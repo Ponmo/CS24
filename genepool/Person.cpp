532f55ad00f8eb72e463840db814543434f2d613
Noah Wang <noahwang@169-231-97-66.wireless.ucsb.edu>
2022-11-02 16:41:29 -0700

    ef

--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -167,16 +167,9 @@ std::set<Person*> Person::grandmothers(PMod pmod) {
 //TODO
 std::set<Person*> Person::grandparents(PMod pmod) { // Maternal Grandparents Return Your Mothers Parents. Paternal Grandparents return your father's parent. All should return both.
     std::set<Person*> result;
-    if(pmod == PMod::MATERNAL || pmod == PMod::ANY) {
-        std::set<Person*> resultOne = grandmothers(pmod);
-        result.insert(resultOne.begin(), resultOne.end());
-        resultOne = grandfathers(pmod);
-        result.insert(resultOne.begin(), resultOne.end());
-        return result;
-    }
-    std::set<Person*> resultOne = grandfathers(pmod);
+    std::set<Person*> resultOne = grandmothers(pmod);
     result.insert(resultOne.begin(), resultOne.end());
-    resultOne = grandmothers(pmod);
+    resultOne = grandfathers(pmod);
     result.insert(resultOne.begin(), resultOne.end());
     return result;
 }
