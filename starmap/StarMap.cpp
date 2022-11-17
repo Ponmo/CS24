6b7f6ad1245b392cb31fd7443c758dc629371501
Noah Wang <noahwang@169-231-104-103.wireless.ucsb.edu>
2022-11-16 16:36:15 -0800

    ef

--- starmap/StarMap.cpp
+++ starmap/StarMap.cpp
@@ -2,11 +2,21 @@
 #include <sstream>
 #include <iostream>
 #include <queue>
+#include <map>
+#include <set>
+#include <stack>
+// #include <cassert>
+#include <cmath>
 // template <class T, class Container = std::vector<T>,  class Compare = std::less<typename Container::value_type> > class priority_queue;
 
+struct starDistance {
+    float distance;
+    int id;
+};
 
+//Find if it is a disk or whatever
 StarMap::StarMap(std::istream& stream) {
-  data = new std::vector<Star>;
+  data = new std::vector<Star>; 
   std::string line;
   int id = 1;
   while (std::getline(stream, line)) {
@@ -36,7 +46,7 @@ StarMap::~StarMap() {
   delete data;
 }
 
-//Use floats, don't use square roots, kevin used multiply (power)
+//Use floats, don't use square roots, kevin used multiply (power), mutliple kd trees for each block
 
 StarMap* StarMap::create(std::istream& stream) {
   // This default implementation will probably do what you want.
@@ -44,13 +54,54 @@ StarMap* StarMap::create(std::istream& stream) {
   return new StarMap(stream);
 }
 
+struct CompareAge {
+    bool operator()(starDistance const & p1, starDistance const & p2) {
+        return p1.distance < p2.distance;
+    }
+};
 
 std::vector<Star> StarMap::find(size_t n, float x, float y, float z) { //find function
-  // priority_queue<Star, std::vector<Star>, std::greater<int>> nearStars;
-  std::priority_queue<Star> nearestStars;
-  x++;
-  y++;
-  z++;
-  std::vector<Star> hello;
-  return hello;
+  std::priority_queue<starDistance, std::vector<starDistance>, CompareAge> pq;
+  for(Star star : *data) {
+    float distance = sqrt(pow(star.x - x, 2) + pow(star.y-y, 2) + pow(star.z-z, 2));
+    starDistance curr = {distance, star.id};
+    if(pq.size() >= n && pq.top().distance > distance) {
+      pq.pop();
+      pq.push(curr);
+    }
+    else {
+      pq.push(curr);
+    }
+  }
+  std::vector<Star> nearest;
+  for(size_t i = 0; i < n; i++) {
+    nearest.push_back(data->at(pq.top().id));
+    pq.pop();
+  }
+  return nearest;
 };
+
+
+
+
+// std::vector<Star> StarMap::find(size_t n, float x, float y, float z) { //find function
+//   float highestDistance = 100000000;
+//   std::vector<std::pair<Star, float>> closest;
+
+//   for(Star star : *data) {
+//     float distance = sqrt(pow(star.x - x, 2) + pow(star.y-y, 2) + pow(star.z-z, 2));
+//     closest.push_back({star, distance});
+//     }
+//   sort(closest.begin(), closest.end());
+//   std::vector<Star> stars;
+//   int count = 0;
+//   for (auto s : closest) {
+//     std::cout << s.first.id;
+//     std::cout << s.second;
+//     std::cout << "\n";
+//     if (count != n) {
+//       stars.push_back(s.first);
+//     }
+//   }
+//   return stars;
+// };
