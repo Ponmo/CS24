7e69bd1fa144aad6ff6ff716f9bbce0085c00407
Noah Wang <noahwang@169-231-104-103.wireless.ucsb.edu>
2022-11-16 16:56:14 -0800

    ef

--- starmap/StarMap.cpp
+++ starmap/StarMap.cpp
@@ -26,16 +26,20 @@ StarMap::StarMap(std::istream& stream) {
     star.id = id;
     int count = 0;
     while(std::getline(nLine, token, '\t')) {
-      std::cout << token;
+      // std::cout << token;
       if (count == 0) {
+        std::cout << '1';
         star.x = std::stof(token);
       }
       if (count == 1) {
+        std::cout << '2';
         star.y = std::stof(token);
       }
       if (count == 2) {
+        std::cout << '3';
         star.z = std::stof(token);
       }
+      count++;
     }
     data->push_back(star);
     id++;
@@ -63,7 +67,7 @@ struct CompareAge {
 std::vector<Star> StarMap::find(size_t n, float x, float y, float z) { //find function
   std::priority_queue<starDistance, std::vector<starDistance>, CompareAge> pq;
   for(Star star : *data) {
-    float distance = sqrt(pow(star.x - x, 2) + pow(star.y-y, 2) + pow(star.z-z, 2));
+    float distance = sqrt(pow(star.x - x, 2) + pow(star.y - y, 2) + pow(star.z - z, 2));
     starDistance curr = {distance, star.id};
     if(pq.size() >= n && pq.top().distance > distance) {
       pq.pop();
@@ -75,7 +79,9 @@ std::vector<Star> StarMap::find(size_t n, float x, float y, float z) { //find fu
   }
   std::vector<Star> nearest;
   for(size_t i = 0; i < n; i++) {
-    nearest.push_back(data->at(pq.top().id));
+    std::cout << pq.top().id;
+    std::cout << " ";
+    nearest.push_back(data->at(pq.top().id - 1));
     pq.pop();
   }
   return nearest;
