2799984dfbc0b03ad11868ff472d9723caf75f60
Noah Wang <noahwang@169-231-104-103.wireless.ucsb.edu>
2022-11-20 22:01:19 -0800

    a

--- starmap/StarMap.cpp
+++ starmap/StarMap.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <fstream>
 #include <algorithm>
-#include <time.h>
 
 bool comparatorx(const Star& lhs, const Star& rhs) {
    return lhs.x < rhs.x;
@@ -41,7 +40,7 @@ StarMap::StarMap(std::istream& stream) {
     data.push_back(star);
     id++;
   } //Approx Variance with randomly selected stars, setting beginning depth to that, 
-  std::srand(time(0));
+  //std::srand(time(0));
   Star A;
   float xAvg = 0;
   float yAvg = 0;
@@ -64,42 +63,10 @@ StarMap::StarMap(std::istream& stream) {
   xAvgSq = xAvgSq/500;
   yAvgSq = yAvgSq/500;
   zAvgSq = zAvgSq/500;
-  // Star A = data.at(std::rand()%(data.size()));
-  // Star B = data.at(std::rand()%(data.size()));
-  // Star C = data.at(std::rand()%(data.size()));
-  // Star D = data.at(std::rand()%(data.size()));
-  // Star E = data.at(std::rand()%(data.size()));
-  // Star F = data.at(std::rand()%(data.size()));
-  // Star G = data.at(std::rand()%(data.size()));
-  // Star H = data.at(std::rand()%(data.size()));
-  // Star I = data.at(std::rand()%(data.size()));
-  // Star J = data.at(std::rand()%(data.size()));
-  // Star K = data.at(std::rand()%(data.size()));
-  // Star L = data.at(std::rand()%(data.size()));
-  // Star M = data.at(std::rand()%(data.size()));
-  // Star N = data.at(std::rand()%(data.size()));
-  // Star O = data.at(std::rand()%(data.size()));
-  // Star P = data.at(std::rand()%(data.size()));
-  // Star Q = data.at(std::rand()%(data.size()));
-  // Star R = data.at(std::rand()%(data.size()));
-  // Star S = data.at(std::rand()%(data.size()));
-  // Star T = data.at(std::rand()%(data.size()));
-  // Star U = data.at(std::rand()%(data.size()));
-  // Star V = data.at(std::rand()%(data.size()));
-  // Star W = data.at(std::rand()%(data.size()));
-  // Star X = data.at(std::rand()%(data.size()));
-  // Star Y = data.at(std::rand()%(data.size()));
-  // Star Z = data.at(std::rand()%(data.size()));
-  // float xAvg = (A.x+B.x+C.x+D.x+E.x+F.x+G.x+H.x+I.x+J.x+K.x+L.x+M.x+N.x+O.x+P.x+Q.x+R.x+S.x+T.x+U.x+V.x+W.x+X.x+Y.x+Z.x)/26;
-  // float yAvg = (A.y+B.y+C.y+D.y+E.y+F.y+G.y+H.y+I.y+J.y+K.y+L.y+M.y+N.y+O.y+P.y+Q.y+R.y+S.y+T.y+U.y+V.y+W.y+X.y+Y.y+Z.y)/26;
-  // float zAvg = (A.z+B.z+C.z+D.z+E.z+F.z+G.z+H.z+I.z+J.z+K.z+L.z+M.z+N.z+O.z+P.z+Q.z+R.z+S.z+T.z+U.z+V.z+W.z+X.z+Y.x+Z.z)/26;
-  // float xAvgSq = (A.x*A.x+B.x*B.x+C.x*C.x+D.x*D.x+E.x*E.x+F.x*F.x+G.x*G.x+H.x*H.x+I.x*I.x+J.x*J.x+K.x*K.x+L.x*L.x+M.x*M.x+N.x*N.x+O.x*O.x+P.x*P.x+Q.x*Q.x+R.x*R.x+S.x*S.x+T.x*T.x+U.x*U.x+V.x*V.x+W.x*W.x+X.x*X.x+Y.x*Y.x+Z.x*Z.x)/26;
-  // float yAvgSq = (A.y*A.y+B.y*B.y+C.y*C.y+D.y*D.y+E.y*E.y+F.y*F.y+G.y*G.y+H.y*H.y+I.y*I.y+J.y*J.y+K.y*K.y+L.y*L.y+M.y*M.y+N.y*N.y+O.y*O.y+P.y*P.y+Q.y*Q.y+R.y*R.y+S.y*S.y+T.y*T.y+U.y*U.y+V.y*V.y+W.y*W.y+X.y*X.y+Y.y*Y.y+Z.y*Z.y)/26;
-  // float zAvgSq = (A.z*A.z+B.z*B.z+C.z*C.z+D.z*D.z+E.z*E.z+F.z*F.z+G.z*G.z+H.z*H.z+I.z*I.z+J.z*J.z+K.z*K.z+L.z*L.z+M.z*M.z+N.z*N.z+O.z*O.z+P.z*P.z+Q.z*Q.z+R.z*R.z+S.z*S.z+T.z*T.z+U.z*U.z+V.z*V.z+W.z*W.z+X.z*X.z+Y.z*Y.z+Z.z*Z.z)/26;
   float xVar = std::abs(xAvgSq - xAvg*xAvg);
   float yVar = std::abs(yAvgSq - yAvg*yAvg);
   float zVar = std::abs(zAvgSq - zAvg*zAvg);
-  // std::cout << xVar << " " << yVar << " " << " " << zVar << "\n";
+  std::cout << xVar << " " << yVar << " " << " " << zVar << "\n";
   if(xVar >= yVar && xVar >= zVar) { //X is largest
     begDepth = 0;
   }
